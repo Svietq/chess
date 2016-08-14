789f016b98900fcbce30fab3212a74dcd7ab717e
Svietq <mswietku@gmail.com>
2016-08-14 18:06:02 +0200

    Create rook.cpp

---
+++ rook.cpp
@@ -0,0 +1,25 @@
+#include "rook.h"
+
+void Rook::move(Piece_coords icoords)
+{
+    if( is_move_correct(icoords))
+    {
+        _coords = icoords;
+        _is_moved = true;
+    }
+}
+
+bool Rook::is_move_correct(Piece_coords icoords)
+{
+    if( (_coords.x==icoords.x && _coords.y!=icoords.y) || (_coords.x!=icoords.x && _coords.y==icoords.y) )
+    {
+        return true;
+    }
+
+    return false;
+}
+
+Piece_type Rook::get_type()
+{
+    return _type;
+}
