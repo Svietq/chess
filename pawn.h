e3fdba6096b07a743805bd0a94701e713f8d072b
Svietq <mswietku@gmail.com>
2016-08-14 17:55:45 +0200

    Create pawn.h

---
+++ pawn.h
@@ -0,0 +1,19 @@
+#ifndef PAWN_H
+#define PAWN_H
+
+#include "piece.h"
+
+class Pawn : public Piece
+{
+    Piece_type _type;
+
+public:
+    Pawn(Piece_color icolor) : Piece(icolor), _type(Piece_type::PAWN) { }
+
+    virtual void move(Piece_coords icoords);
+    virtual bool is_move_correct(Piece_coords icoords);
+    virtual Piece_type get_type();
+
+};
+
+#endif // PAWN_H
