4f867a7fd1d632b3e899ae6aa626ac2b3ba7c13a
Svietq <mswietku@gmail.com>
2016-08-14 17:53:38 +0200

    Create graphics.h

---
+++ graphics.h
@@ -0,0 +1,26 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+//graphics of the game
+
+#include "console.h"
+#include "types.h"
+
+#include <vector>
+#include <iostream>
+#include <array>
+#include <memory>
+
+using namespace std;
+
+class Graphics : virtual public Console
+{
+public:
+    Graphics();
+
+    template<typename T> void print_board(const array<array<unique_ptr<T>, 8>, 8> &iboard);
+    void print_menu();
+};
+#include "graphics_impl.h" //implementation of template function print_board()
+
+#endif // GRAPHICS_H
