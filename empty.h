2b1e44d0926aa23621522b47e0f5343ce5d8c9e6
Svietq <mswietku@gmail.com>
2016-08-14 17:53:14 +0200

    Create empty.h

---
+++ empty.h
@@ -0,0 +1,19 @@
+#ifndef EMPTY_H
+#define EMPTY_H
+
+//subclass of class piece which represents empty field of the board
+
+#include "piece.h"
+
+class Empty : public Piece
+{
+    Piece_type _type;
+public:
+    Empty() : Piece(), _type(Piece_type::EMPTY) {}
+
+    virtual Piece_type get_type();
+    virtual void move(Piece_coords icoords) {}
+    virtual bool is_move_correct(Piece_coords icoords){}
+};
+
+#endif // EMPTY_H
