8e74a3045caf82baa0d83872e9dfbe666aa2739e
Svietq <mswietku@gmail.com>
2016-08-21 13:40:05 +0200

    Update bishop.h

--- bishop.h
+++ bishop.h
@@ -9,10 +9,12 @@ class Bishop : public Piece
 
 public:
     Bishop(Piece_color icolor) : Piece(icolor), _type(Piece_type::BISHOP) {}
+    Bishop(Piece_coords icoords, Piece_color icolor) : Piece(icoords, icolor), _type(Piece_type::BISHOP) {}
 
     virtual void move(Piece_coords icoords);
     virtual bool is_move_correct(Piece_coords icoords);
     virtual Piece_type get_type();
+    virtual bool is_empty();
 };
 
 #endif // BISHOP_H
