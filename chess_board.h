f13385f7684e5ecd83103f0ab7b92d61fd6322b5
Svietq <mswietku@gmail.com>
2016-08-21 13:41:55 +0200

    Create chess_board.h

---
+++ chess_board.h
@@ -0,0 +1,24 @@
+#ifndef CHESS_BOARD_H
+#define CHESS_BOARD_H
+
+#include "types.h"
+#include "piece.h"
+#include <iterator>
+
+using namespace std;
+
+class Chess_board
+{
+    Pieces_board _board; //8x8 array representing the board with pieces
+public:
+    Chess_board();
+
+    unique_ptr<Piece>& operator[](const Piece_coords &icoords);
+    const unique_ptr<Piece>& operator[](const Piece_coords &icoords) const;
+    Pieces_board::iterator begin();
+    Pieces_board::iterator end();
+
+    const Pieces_board &get();
+};
+
+#endif // CHESS_BOARD_H
