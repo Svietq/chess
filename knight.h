79a398bdc8b983056d67c2cc47fdf5d9cade1cb6
Svietq <mswietku@gmail.com>
2016-08-21 13:46:51 +0200

    Update knight.h

--- knight.h
+++ knight.h
@@ -9,10 +9,13 @@ class Knight : public Piece
 
 public:
     Knight(Piece_color icolor) : Piece(icolor), _type(Piece_type::KNIGHT) {}
+    Knight(Piece_coords icoords, Piece_color icolor) : Piece(icoords, icolor), _type(Piece_type::KNIGHT) {}
 
     virtual void move(Piece_coords icoords);
     virtual bool is_move_correct(Piece_coords icoords);
     virtual Piece_type get_type();
+    virtual bool is_empty();
+
 };
 
 #endif // KNIGHT_H
