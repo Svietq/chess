96db871283289c5256f8ad8089abd2469e66ba4f
Svietq <mswietku@gmail.com>
2016-08-21 13:46:15 +0200

    Update king.h

--- king.h
+++ king.h
@@ -9,10 +9,12 @@ class King  : public Piece
 
 public:
     King(Piece_color icolor) : Piece(icolor), _type(Piece_type::KING) {_is_moved = false;}
+    King(Piece_coords icoords, Piece_color icolor) : Piece(icoords, icolor), _type(Piece_type::KING) {}
 
     virtual void move(Piece_coords icoords);
     virtual bool is_move_correct(Piece_coords icoords);
     virtual Piece_type get_type();
+    virtual bool is_empty();
 };
 
 #endif // KING_H
