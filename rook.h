4ae70d2e45adcc2adfd26352613c996caa920fc3
Svietq <mswietku@gmail.com>
2016-08-21 13:48:49 +0200

    Update rook.h

--- rook.h
+++ rook.h
@@ -9,10 +9,12 @@ class Rook : public Piece
 
 public:
     Rook(Piece_color icolor) : Piece(icolor), _type(Piece_type::ROOK) {_is_moved = false;}
+    Rook(Piece_coords icoords, Piece_color icolor) : Piece(icoords, icolor), _type(Piece_type::ROOK) {}
 
     virtual void move(Piece_coords icoords);
     virtual bool is_move_correct(Piece_coords icoords);
     virtual Piece_type get_type();
+    virtual bool is_empty();
 
 };
 
