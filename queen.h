0c2dcda6836ff76333b9ac12a2eab7724560163d
Svietq <mswietku@gmail.com>
2016-08-21 13:48:20 +0200

    Update queen.h

--- queen.h
+++ queen.h
@@ -9,10 +9,12 @@ class Queen  : public Piece
 
 public:
     Queen(Piece_color icolor) : Piece(icolor), _type(Piece_type::QUEEN) {}
+    Queen(Piece_coords icoords, Piece_color icolor) : Piece(icoords, icolor), _type(Piece_type::QUEEN) {}
 
     virtual void move(Piece_coords icoords);
     virtual bool is_move_correct(Piece_coords icoords);
     virtual Piece_type get_type();
+    virtual bool is_empty();
 
 };
 
