65e2403530b6c25a17024390f1e207ef9cced0b5
Svietq <mswietku@gmail.com>
2016-08-21 13:49:22 +0200

    Update types.h

--- types.h
+++ types.h
@@ -3,6 +3,11 @@
 
 //file that holds user-defined types
 
+#include <array>
+#include <memory>
+
+class Piece;
+using Pieces_board = std::array<std::unique_ptr<Piece>, 64 >;
 enum class Piece_color : bool{WHITE=0, BLACK=1};
 enum class Piece_type : int{EMPTY=0, PAWN=1, KNIGHT=2, BISHOP=3, ROOK=4, QUEEN=5, KING=6 };
 struct Piece_coords
@@ -18,5 +23,7 @@ inline bool operator==(const Piece_coords& lhs, const Piece_coords& rhs)
 
     return false;
 }
+
+
 
 #endif // TYPES_H
