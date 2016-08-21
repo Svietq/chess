81fba0300468f393410db51a85dae917d3f8b020
Svietq <mswietku@gmail.com>
2016-08-21 13:44:39 +0200

    Update empty.h

--- empty.h
+++ empty.h
@@ -10,10 +10,12 @@ class Empty : public Piece
     Piece_type _type;
 public:
     Empty() : Piece(), _type(Piece_type::EMPTY) {}
+    Empty(Piece_coords icoords) : Piece(icoords), _type(Piece_type::EMPTY) {}
 
     virtual Piece_type get_type();
     virtual void move(Piece_coords icoords) {}
     virtual bool is_move_correct(Piece_coords icoords){}
+    virtual bool is_empty();
 };
 
 #endif // EMPTY_H
