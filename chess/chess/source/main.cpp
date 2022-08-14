ac3ddaafffb4d80b16d26d99e7c9b87676aa0d1f
svietq <mswietku@gmail.com>
2022-08-14 13:49:44 +0200

    Refactor main.cpp

--- chess/chess/source/main.cpp
+++ chess/chess/source/main.cpp
@@ -1,13 +1,8 @@
 #include "chess_game.h"
 
-#include <iostream>
 
-using namespace std;
-
-int main(int argc, char *argv[])
+int main()
 {
     Chess_game game;
     game.play();
-
-    return 0;
 }
